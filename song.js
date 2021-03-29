2c383384b96485f06c39c6da14787862712e65c7
Kyung Mo Yang <kyungmoyang9091.92@gmail.com>
2021-03-29 23:50:35 +0200

    do homework 1

---
+++ song.js
@@ -0,0 +1,72 @@
+// Chosen song : 'Spring Day', from BTS
+
+/*
+This song was dedicated to children who were killed during the Saewol ferry case.
+The tragic accident took place in 2014.
+The ferry departed from Incheon and was heading for Jeju.
+Out of the 476 passengers and crew members, 304 of them were found dead.
+Many of them were high school students on a school trip.
+*/
+
+const chorus = "눈꽃이 떨어져요\n" +
+    "또 조금씩 멀어져요\n" +
+    "보고 싶다 (보고 싶다)\n" +
+    "보고 싶다 (보고 싶다)\n" +
+    "얼마나 기다려야\n" +
+    "또 몇 밤을 더 새워야\n" +
+    "널 보게 될까 (널 보게 될까)\n" +
+    "만나게 될까 (만나게 될까, ah-ah-ah-ah, ah)\n\n";
+
+const lyrics = "보고 싶다\n" + 
+    "이렇게 말하니까 더 보고 싶다\n" +
+    "너희 사진을 보고 있어도\n" +
+    "보고 싶다\n" +
+    "너무 야속한 시간, 나는 우리가 밉다\n" +
+    "이젠 얼굴 한 번 보는 것 조차, 힘들어진 우리가\n" +
+    "여긴 온통 겨울 뿐이야, 팔월에도 겨울이 와\n" +
+    "마음은 시간을 달려가네, 홀로 남은 설국열차\n" +
+    "니 손 잡고 지구 반대편까지 가, 겨울을 끝내고파\n" +
+    "그리움들이 얼마나 눈처럼 내려야, 그 봄날이 올까, friend\n\n" +
+    "허공을 떠도는\n" +
+    "작은 먼지처럼, 작은 먼지처럼\n" +
+    "날리는 눈이 나라면\n" +
+    "조금 더 빨리 네게\n" + 
+    "닿을 수 있을 텐데\n\n" + chorus +
+    "추운 겨울 끝을 지나 다시 봄날이 올 때까지\n" + 
+    "꽃 피울 때까지\n" + 
+    "그곳에 좀 더 머물러줘, 머물러줘\n\n" +
+    "니가 변한 건지 (니가 변한 건지)\n" + 
+    "아니면 내가 변한 건지 (아니면 내가 변한 건지)\n" + 
+    "이 순간 흐르는 시간조차 미워\n" + 
+    "우리가 변한, 거지 뭐? 모두가 그런, 거지 뭐?\n" + 
+    "그래 밉다 니가, 넌 떠났지만\n" + 
+    "단 하루도 너를, 잊은 적이 없었지 난\n" + 
+    "솔직히, 보고 싶은데, 이만 너를 지울게\n" + 
+    "그게 널 원망하기보단 덜 아프니까\n\n" +
+    "시린 널 불어내 본다\n" + 
+    "연기처럼, 하얀 연기처럼\n" + 
+    "말로는 지운다 해도\n" + 
+    "사실 난 아직 널 보내지 못하는데\n\n" + chorus +
+    "You know it all, you're my best friend\n" + 
+    "아침은 다시 올 거야\n" + 
+    "어떤 어둠도 어떤 계절도\n" + 
+    "영원할 순 없으니까\n\n" + chorus +
+    "추운 겨울 끝을 지나 다시 봄날이 올 때까지\n" + 
+    "꽃 피울 때까지\n" + 
+    "그곳에 좀 더 머물러줘, 머물러줘";
+
+    let song = {
+        titleEng: "Spring Day",
+        titleOrig: "봄날",
+        genre: "k-pop",
+        artist: "BTS",
+        releaseDate: "Feb 12. 2017",
+        duration: 275,
+        language: "KR",
+        album: "You Never Walk Alone",
+        lyrics: lyrics,
+        receivedNominations: true,
+        hitTheChart: true,
+    }
+
+    console.log(song);
